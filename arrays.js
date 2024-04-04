fb6a029736f60ba0d76cf176e5ef0f976030be4f
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-04-04 14:04:20 +0000

    lalala

--- arrays.js
+++ arrays.js
@@ -1,40 +1,40 @@
 // CODE ALONG
 
 // 1. Update the variable songList so that it is an array with at least 3 song titles.
-let songList;
-console.log();
+let songList = ["abcd", "efg", "hijk"];
+console.log(songList);
 
 // 2. Update the variable studentList so that it is an array with 4 Code Nation students (including yourself) in your class.
-let studentList;
-console.log();
+let studentList= ["jake", "charlie", "Jo"];
+console.log(studentList);
 
 // 3. Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
-let teacherList;
-console.log();
+let teacherList = ["Mx. Dirr", "Ms. Gartland", "Ms. Lindeman"];
+console.log(teacherList);
 
 // 4. Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
-let familyAges;
-console.log();
+let familyAges = ["12", "13", "14"];
+console.log(familyAges);
 
 //PAUSE HERE
 
 // CODE SOLO
 
 // 5. Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
-let favSong;
-console.log();
+let favSong = ["lmno", "pqrs", "tuv"];
+console.log(favSong);
 
 // 6. Update the variable bestStudent to retrieve your name from the classList array (Task 2).
-let bestStudent;
-console.log();
+let bestStudent=["coolStudent", "superStudent", "wowStudent"]
+console.log(bestStudent);
 
 // 7. Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
-let tallestTeacher;
-console.log();
+let tallestTeacher= ["giantTeacher", "buildingTeacher", "superTallTeacher"];
+console.log(tallestTeacher);
 
 // 8. Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
 let oldestMember;
-console.log();
+console.log(oldestMember);
 
 // Done Early?
 //  - On your own, create an array that combines both the class list and the teacher list without rewriting the names of students and teachers.
